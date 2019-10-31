1441bd2661372fc8c279cccc9093c1883da6d78b
Philippe Castelli <philippe.castelli@gmail.com>
2019-10-31 18:21:56 +0100

    style(form): cleanup

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -6,37 +6,33 @@ import { HorizontalFormGroup } from './formGroup/HorizontalFormGroup';
 const HORIZONTAL_FORM_MIN_WIDTH = 350;
 
 interface BlockProps {
-  id: string;
+  id: string;
   className?: string;
 }
 
-export const Block: React.FC<BlockProps> = ({id, className}) => {
+export const Block: React.FC<BlockProps> = ({ id, className }) => {
   const cardEl = React.useRef<HTMLDivElement>(null);
   const [isSmallBlock, setSmallBlock] = React.useState(false);
 
   React.useLayoutEffect(() => {
     const cardWidth = cardEl.current.clientWidth;
-    if (cardWidth < HORIZONTAL_FORM_MIN_WIDTH) {
-      setSmallBlock(true);
-    }
-    else setSmallBlock(false);
+    setSmallBlock(cardWidth < HORIZONTAL_FORM_MIN_WIDTH);
   })
 
-  // const FormGroup = HorizontalFormGroup;
-  const FormGroup = isSmallBlock ? VerticalFormGroup : HorizontalFormGroup;
+  const FormGroup = isSmallBlock ? VerticalFormGroup : HorizontalFormGroup;
 
   return (
-    <div className={classNames('my-3', className || 'col-md-6'}>
+    <div className={classNames('my-3', className || 'col-md-6')}>
       <div className="card shadow bg-white rounded" ref={cardEl}>
         <div className="card-body">
           <h5 className="card-title">{id}</h5>
           <div className="card-text">
             {
-              [1,2,3].map(fieldIndex => {
+              [1, 2, 3].map(fieldIndex => {
                 const fieldId = `${id}-field-${fieldIndex}`;
                 return (
                   <FormGroup inputId={fieldId} key={fieldIndex}>
-                    <input type="text" id={fieldId} className="form-control"/>
+                    <input type="text" id={fieldId} className="form-control" />
                   </FormGroup>
                 )
               })
--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import styled from 'styled-components';
 import { getBreakpoint } from './hooks/useWindowSize';
 
-export const SidePanel: React.FC = () => {
+export const SidePanel: React.FC = () => {
   const lgBreakpoint = getBreakpoint('lg');
-  
+
   const PanelDiv = React.useMemo(() => styled.div`
   min-height: 300px;
 
@@ -12,7 +12,7 @@ export const SidePanel: React.FC = () => {
     margin-top: 1rem;
   }
   `, []);
-  
+
   return (
     <PanelDiv className="card shadow bg-primary h-100">
       <div className="card-body">
